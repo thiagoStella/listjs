268e0ae3a50057d558b1eb9c9c788130e70eff06
Thiago Stella Pontes <thiagostellapontes01@gmail.com>
2023-04-05 15:49:45 -0300

    concluido explicação
    Co-authored-by: Clemilton Lima de Souza <Clemiltonls-prof@users.noreply.github.com>

---
+++ tutorial.js
@@ -0,0 +1,88 @@
+
+$("#demo").guides({
+    guides: [
+        {
+            element: $('.texto-logo'),
+            html: 'Plugin para geração de listas dinâmicas'
+        },
+        {
+            element: $('.overview'),
+            html: 'Menu lateral para separar o conteúdo'
+        },
+        {
+            element: $('.overview'),
+            html: 'Neste sub menu, contém informações sobre testes automatizados e contribuições em geral'
+        },
+        {
+            element: $('.documentation'),
+            html: 'Neste sub menu temos documentações diversas e duvidas frequentes'
+        },
+        {
+            element: $('.examples'),
+            html: 'E nesta ultima sub categoria temos os exemplos de usabilidade'
+        },
+    ]
+});
+
+
+$("#demo2").guides({
+    guides: [
+        {
+            element: $('.search'),
+            html: 'Campo para buscas'
+        },
+        {
+            element: $('.edit-item-btn'),
+            html: 'Botão para editar'
+        },
+        {
+            element: $('.remove-item-btn'),
+            html: 'Botão para excluir'
+        },
+        {
+            element: $('#name-field'),
+            html: 'Campo para adicionar nos itens à lista'
+        },
+        
+    ]
+});
+
+$("#demo3").guides({
+    guides: [
+        {
+            element: $('.body'),
+            html: 'Opções de download'
+        },
+        {
+            element: $('.github'),
+            html: 'Código-fonte'
+        },
+        {
+            element: $('.b1'),
+            html: 'Baixar arquivo compactado'
+        },
+        {
+            element: $('.b2'),
+            html: 'Baixar arquivo não compactado'
+        },
+        {
+            element: $('.npm'),
+            html: 'Gerenciadores de pacotes'
+        },
+        {
+            element: $('.bower'),
+            html: 'Gerenciadores de pacotes'
+        },
+        {
+            element: $('.cdn'),
+            html: 'link no próprio código'
+        },
+        {
+            element: $('.menu'),
+            html: 'para mais informações acesse o site oficial'
+        },
+        
+    ]
+});
+
+
